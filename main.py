2e3f06e6cd829aaf83cb68c4682a035a58837b33
sleipnir22 <bolovindenis@gmail.com>
2021-12-23 19:03:32 +0700

    Учел ошибки. Тестовый ключ и сессионный ключ при расшифроке не совпадают

--- main.py
+++ main.py
@@ -1,81 +1,89 @@
 import rsa
 import aes
 import hashlib
+from sys import getsizeof
 
 from flask import Flask, request
 
 app = Flask(__name__)
 
-#TEST_KEY = 3480299736719771887772948464190228533233280192811212880921  # 192 bits
-session_private_key = int
+TEST_KEY = 3480299736719771887772948464190228533233280192811212880921  # 192 bits
+session_key = int
+private_key = rsa.PrivateKey
+cipher = aes.CipherAES
 
 
 @app.route("/api/set_session_key")
 def set_session_key(key: int) -> None:
-    global session_private_key
-    session_private_key = key
+    global session_key, cipher
+    session_key = rsa.decrypt(key, private_key)
+    assert(session_key != TEST_KEY)
+    cipher = aes.CipherAES(session_key)
 
 
 @app.route('/api/eval_msg', methods=['POST'])
 def eval_msg() -> dict:
     if request.method == ['POST']:
         msg = request.form['message']
-        decrypted_message = decrypt_message(msg)
+        hsh = request.form['hash']
+        decrypted_message = cipher.decrypt(msg)
         hashed_message = hash_func(decrypted_message)
+        solved = eval(decrypted_message)
+        encrypted_message = cipher.encrypt(str(solved))
+        return {
+            'hash': hashed_message,
+            'message': encrypted_message
+        }
 
-        if decrypted_message.isnumeric():
-            solved = eval(decrypted_message)
-            encrypted_message = encrypt_message(str(solved))
-            return {
-                'hashed message': hashed_message,
-                'encrypted message': encrypted_message
-            }
-        else:
-            return {
-                'Exception': 'Number expected!'
-            }
 
+def mock_eval_msg(msg: str, hashed: str) -> dict:
+    decrypted_message = cipher.decrypt(msg)
+    hashed_message = hash_func(decrypted_message)
+
+    solved = eval(decrypted_message)
+    encrypted_message = cipher.encrypt(str(solved))
+    return {
+        'hash': hashed_message,
+        'message': encrypted_message
+    }
 
 @app.route("/api/get_public_key")
-def get_public_key(len: int) -> dict:
-    (public_key, _) = rsa.new_keys(len)
-    return {
-        'e': public_key.e,
-        'n': public_key.n
-    }
+def get_public_key(len: int) -> rsa.PublicKey:
+    global private_key
+    (public_key, private_key) = rsa.new_keys(len)
+    return public_key
 
 
 def hash_func(msg: str):
-    result = hashlib.md5(b'{msg}')
-    return result.hexdigest()
+    result = hashlib.md5(msg.encode('utf8'))
+    return str(result.hexdigest())
 
 
-def decrypt_message(msg: str):
-    cipher = aes.CipherAES(session_private_key)
-    decrypted = cipher.decrypt(msg)
-    return decrypted
+def test():
+    test_public_key = get_public_key(192)
+    encrypted_key = rsa.encrypt(TEST_KEY, test_public_key)
+    print(f'encrypted_key : {encrypted_key}')
+    set_session_key(encrypted_key)
 
+    test_messages = ['1 + 1',
+                     '2 * 2',
+                     '3 / 2']
 
-def encrypt_message(msg: str):
-    cipher = aes.CipherAES(session_private_key)
-    encrypted = cipher.encrypt(msg)
-    return encrypted
+    test_hashes = [hash_func(message) for message in test_messages]
+    dictionary = dict(zip(test_messages, test_hashes))
 
-'''@app.route("/")
-def hello_world():
-    cipher = aes.CipherAES(TEST_KEY)
-    msg = "Hi, it's ascii text + русские буквы"
-    encrypt = cipher.encrypt(msg)
-    decrypt = cipher.decrypt(encrypt)
+    for msg, hsh in dictionary.items():
+        pass
+        #print(f'the message is {msg} and the hash is {hsh}')
+    ''' msg = cipher.encrypt(msg)
+        msg_n_hash = mock_eval_msg(msg, hsh)
+        msg = cipher.decrypt(msg_n_hash['msg'])'''
 
-    return {
-        "source_msg": msg,
-        "encrypt": encrypt,
-        "decrypt": decrypt,
-    }'''
 
 if __name__ == '__main__':
+    test()
     app.run(debug=True)
+
 
 @app.route("/api/mirror")
 def mirror(msg):
