2739a2a1eb3ff9fb061f01fbebe25574cf376a21
User <ju.fila9@gmail.com>
2021-12-24 22:30:10 +0700

    /

--- main.py
+++ main.py
@@ -1,36 +1,90 @@
 import rsa
 import aes
+import hashlib
 
-from flask import Flask, render_template
+from flask import Flask, request
 
 app = Flask(__name__)
 
 TEST_KEY = 3480299736719771887772948464190228533233280192811212880921  # 192 bits
+private_key: rsa.PrivateKey
+cipher: aes.CipherAES
 
 
 @app.route("/")
-def hello_world():
-    cipher = aes.CipherAES(TEST_KEY)
-    msg = "Hi, it's ascii text + русские буквы"
-    encrypt = cipher.encrypt(msg)
-    decrypt = cipher.decrypt(encrypt)
+def home() -> str:
+    return "Hi"
+
+
+@app.route("/api/set_session_key")
+def set_session_key(key: int) -> str:
+    try:
+        global cipher
+        session_key = rsa.decrypt(key, private_key)
+        cipher = aes.CipherAES(session_key)
+    except Exception:
+        return "Error!"
+    return "Session key set successfully"
+
+
+@app.route("/api/request_msg", methods=["POST"])
+def request_msg() -> dict:
+    if request.method == ["POST"]:
+        message = request.form["message"]
+        hash = request.form["hash"]
+        return eval_msg(message, hash)
+
+
+def eval_msg(msg: str, hsh: str) -> dict:
+    try:
+        decrypted_message = cipher.decrypt(msg)
+        if hash_func(decrypted_message) != hsh:
+            raise ValueError("hash functions don't match")
+        message = str(eval(decrypted_message))
+    except Exception as e:
+        message = str(e)
 
     return {
-        "source_msg": msg,
-        "encrypt": encrypt,
-        "decrypt": decrypt,
+        "hash": hash_func(message),
+        "message": cipher.encrypt(message),
     }
 
 
-@app.route("/api/mirror/<msg>")
-def mirror(msg):
-    return msg
+@app.route("/api/get_public_key")
+def get_public_key(length: int) -> rsa.PublicKey:
+    global private_key
+    (public_key, private_key) = rsa.new_keys(length)
+    return public_key
 
 
-@app.route("/home")
-def connect():
-    return render_template("connect.html")
+def hash_func(msg: str):
+    result = hashlib.md5(msg.encode("utf8"))
+    return str(result.hexdigest())
+
+
+def test(input, expected):
+    encrypted_key = rsa.encrypt(TEST_KEY, get_public_key(512))
+    set_session_key(encrypted_key)
+
+    hsh = hash_func(input)
+    msg = cipher.encrypt(input)
+
+    dictionary = eval_msg(msg, hsh)
+    recieved_enc_message = dictionary["message"]
+    recieved_hash = dictionary["hash"]
+    recieved_message = cipher.decrypt(recieved_enc_message)
+
+    assert recieved_message == expected
+    assert hash_func(recieved_message) == recieved_hash
 
 
 if __name__ == "__main__":
+    test("1 + 2 * 3", "7")
+    test("1//////3", "invalid syntax (<string>, line 1)")
+    test("error", "name 'error' is not defined")
     app.run(debug=True)
+
+
+@app.route("/api/mirror")
+def mirror(msg):
+    return msg
